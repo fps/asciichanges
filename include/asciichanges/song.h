cbae77648778ab62aed1e94a685eeef15d752740
Florian Paul Schmidt <mista.tapas@gmx.net>
2015-09-28 15:27:19 +0200

    add test 003

--- include/asciichanges/song.h
+++ include/asciichanges/song.h
@@ -1,9 +1,43 @@
 #pragma once
 
+#include <boost/spirit/include/qi.hpp>
+
+#include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/spirit/include/phoenix_bind.hpp>
+
+#include <ostream>
+
 #include <asciichanges/chords.h>
 
 namespace asciichanges
 {
+    struct song
+    {
 
+    };
+
+    template<typename Iterator>
+    struct song_ : qi::grammar<Iterator, song()>
+    {
+        song_() :
+            song_::base_type(start)
+        {
+            using qi::eps;
+            using qi::val;
+            using qi::_1;
+    
+            start =
+                eps  [ _val = song() ] >>
+                *(
+                    chord_
+                    |
+                    keyvalue_
+                    |
+                    bars_
+                );
+        }
+    }
+
+    qi::rule<Iterator, song()> start;
 }
 
---
+++ tests/003.cc
@@ -0,0 +1,7 @@
+#include <asciichanges/song.h>
+
+int main(int argc, char *argv[])
+{
+
+}
+
