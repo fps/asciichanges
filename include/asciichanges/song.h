9f245bcff9f068ec98c77e6c35eadad320b17ae0
Florian Paul Schmidt <mista.tapas@gmx.net>
2015-09-28 19:57:40 +0200

    add a skeleton keyvalue parser

--- include/asciichanges/song.h
+++ include/asciichanges/song.h
@@ -8,36 +8,82 @@
 #include <ostream>
 
 #include <asciichanges/chords.h>
+#include <string>
 
 namespace asciichanges
 {
+    struct event
+    {
+        virtual ~event() 
+        { 
+
+        }
+    };
+
+    struct keyvalue
+    {
+        std::string key;
+        std::string value;
+
+        keyvalue(const std::string k = "", const std::string v = "") :
+            key(k),
+            value(v)
+        {
+
+        }
+    };
+
     struct song
     {
 
     };
 
     template<typename Iterator>
+    struct keyvalue_ : qi::grammar<Iterator, keyvalue()>
+    {
+        keyvalue_() :
+            keyvalue_::base_type(start)
+        {
+            using qi::eps;
+            using qi::_val;
+            using qi::_1;
+            using qi::alnum;
+ 
+            start = 
+                qi::eps [ _val = keyvalue() ] >>
+                (*alnum >> ':' >> *alnum);
+        }
+
+        qi::rule<Iterator, keyvalue()> start;
+    };
+        
+
+    template<typename Iterator>
     struct song_ : qi::grammar<Iterator, song()>
     {
+        chord_<Iterator> chord;
+        keyvalue_<Iterator> keyvalue;
+        bars_<Iterator> bars;
+
         song_() :
             song_::base_type(start)
         {
             using qi::eps;
-            using qi::val;
+            using qi::_val;
             using qi::_1;
     
             start =
                 eps  [ _val = song() ] >>
                 *(
-                    chord_
+                    chord
                     |
-                    keyvalue_
+                    keyvalue
                     |
-                    bars_
+                    bars
                 );
         }
-    }
 
-    qi::rule<Iterator, song()> start;
+        qi::rule<Iterator, song()> start;
+    }
 }
 
