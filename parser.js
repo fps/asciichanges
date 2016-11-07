ef18d2179cfb8f2582f20a398d8a5476b6439067
Florian Paul Schmidt <mista.tapas@gmx.net>
2016-11-07 22:44:44 +0100

    parse some chords

--- parser.js
+++ parser.js
@@ -1,6 +1,88 @@
 var peg = require('pegjs');
-var asciichanges = require('./asciichanges.js');
+var asciichanges = peg.generate(`
 
-doc = 'Cm79';
+{
+    function noteLetterToInt(c) {
+        if (c === 'C') { 
+            return 0;
+        }
+        if (c === 'D') { 
+            return 2;
+        }
+        if (c === 'E') { 
+            return 4;
+        }
+        if (c === 'F') { 
+            return 5;
+        }
+        if (c === 'G') { 
+            return 7;
+        }
+        if (c === 'A') { 
+            return 9;
+        }
+        if (c === 'B') { 
+            return 11;
+        }
+    }
+}
 
-console.log(asciichanges.parse(doc));
+start
+    =   line*
+
+line
+    =   line:(key_value / chord / bars)? eol
+        { return  line; }
+
+eol
+    =   '\\n'
+
+chord
+    =   note:note type:type? ext:(extension*) { return { chord: { note: note, type: type, extensions: ext } }; }
+
+note
+    =   left:letter right:sharps_or_flats { return { root: left, accidental: right }; }
+
+letter
+    =   letter:[CDEFGAB] { return noteLetterToInt(letter); } 
+
+sharps_or_flats
+    =   sharps / flats
+
+sharps
+    =   bees:('b'*) { return -1 * bees.length; }
+
+flats
+    =   sharps:('#'*) { return sharps.length; }
+
+type
+    =   'm' / 'sus4' / 'sus2' / 'sus' / 'dim' / 'aug'
+
+extension
+    =   '7' / 'b9' / '9' / '#9' / '11' / '#11' / 'b13' / '13'
+
+key_value
+    =   key ":" value
+
+key
+    =   [a-zA-Z]+
+
+value
+    =   [a-zA-Z0-9]+
+
+bars
+    =   bar ( (chord* / ' '+) bar )*
+
+bar
+    =   '|'
+
+`);
+
+doc = `
+C
+D
+Eb
+Dbm79
+`;
+
+console.log(JSON.stringify(asciichanges.parse(doc), null, 4));
