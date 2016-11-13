3b5471639c814f290125fec7a58942401c13678a
Florian Paul Schmidt <mista.tapas@gmx.net>
2016-11-13 22:34:18 +0100

    remove some files

--- test_parser.cmake
+++
@@ -1,8 +0,0 @@
-
-execute_process(COMMAND "${MY_COMMAND}" INPUT_FILE "${INPUT_FILE}" RESULT_VARIABLE RESULT TIMEOUT 1)
-
-if (NOT RESULT STREQUAL "0")
-    message(FATAL_ERROR "exit_failure")
-else()
-    message(INFO "exit_success")
-endif()
